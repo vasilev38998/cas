3fc815c99953e21f350de6814f90f2c7df1018cc
vasilev38998 <vasilev38998@bk.ru>
2026-09-11 21:41:51 +0800

    Add database configuration template

---
+++ app/config.example.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'YOUR_DATABASE_NAME',
+        'user' => 'YOUR_DATABASE_USER',
+        'pass' => 'YOUR_DATABASE_PASSWORD',
+        'charset' => 'utf8mb4',
+    ],
+    'app' => [
+        'name' => 'Candy Club',
+        'base_url' => '',
+        'session_name' => 'candyclub_session',
+        'starting_balance_kopecks' => 1000000,
+    ],
+];
